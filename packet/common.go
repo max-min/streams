672cc766d9ace48f2aac3996eaabaa786aa17e6c
max <zy573124937@163.com>
2019-03-09 21:52:20 +0800

    add ps header api

--- packet/common.go
+++ packet/common.go
@@ -1,15 +1,32 @@
 package packet
 
+const (
+	STREAM_TYPE_H264 = 0x1b
+	STREAM_TYPE_AAC  = 0x90
+)
 
-
+const (
+	STREAM_ID_VIDEO = 0xe0
+	STREAM_ID_AUDIO = 0xc0
+)
  const (
-	START_CODE_PS  = 0x000001ba
-	START_CODE_SYS = 0x000001bb
-	START_CODE_MAP = 0x000001bc
+	START_CODE_PS  		 = 0x000001ba
+	START_CODE_SYS 	   	 = 0x000001bb
+	START_CODE_MAP		 = 0x000001bc
 	START_CODE_PES_VIDEO = 0x000001e0
 	START_CODE_PES_AUDIO = 0x000001c0
 )
 
+//len limit
+const (
+	RTP_HEADER_LENGTH    int = 12
+	PS_HEADER_LENGTH     int = 14
+	SYSTEM_HEADER_LENGTH int = 18
+	MAP_HEADER_LENGTH    int = 24
+	PES_HEADER_LENGTH    int = 19
+	RTP_LOAD_LENGTH      int = 1460
+	PES_LOAD_LENGTH      int = 0xFFFF
+)
 
 /*
  * This implement from VLC source code
@@ -18,13 +35,13 @@ package packet
 
 //BitsBuffer bits buffer
 type BitsBuffer struct {
-	iSize uint32
-	iData uint32
+	iSize int
+	iData int
 	iMask uint8
 	pData []byte
 }
 
-func bitsInit(isize uint32, buffer []byte) *BitsBuffer {
+func bitsInit(isize int, buffer []byte) *BitsBuffer {
 
 	bits := &BitsBuffer{
 		iSize: isize,
@@ -39,11 +56,11 @@ func bitsInit(isize uint32, buffer []byte) *BitsBuffer {
 	return bits
 }
 
-func bitsWrite(bits *BitsBuffer, count uint32, src uint64) *BitsBuffer {
+func bitsWrite(bits *BitsBuffer, count int, src uint64) *BitsBuffer {
 
 	for count > 0 {
 		count--
-		if ((src >> count) & 0x01) != 0 {
+		if ((src >> uint(count)) & 0x01) != 0 {
 			bits.pData[bits.iData] |= bits.iMask
 		} else {
 			bits.pData[bits.iData] &= ^bits.iMask
